fab23039597228b7a6e98f4564c32379b519791b
habib2013 <nairabet25@gmail.com>
2019-07-27 17:20:53 +0100

    New message added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+import { Router } from '@angular/router';
+import * as firebase from 'firebase/app';
+import { AuthserviceService } from './services/authservice.service';
+import { ControlService } from './services/control.service';
+import { ProfileService } from './services/profile.service';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +16,167 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'webadmin';
+  
+  msgVal: string = '';
+  pages: any;
+  username: any;
+  canHide: boolean = false;
+  isAllowed: boolean = false;
+  line: boolean = false;
+  isDarkTheme: boolean = true;
+  constructor(public afAuth: AngularFireAuth, public zone: NgZone, public dialog: MatDialog, public auth: AuthserviceService, private ph:ProfileService, public ctrl: ControlService, public router: Router, public af: AngularFireDatabase) {
+    //localStorage.removeItem('firebase:previous_websocket_failure');
+        this.router.navigateByUrl('').then(sucess =>{
+          
+        })
+     
+    
+    
+ this.pages = [
+  {name: 'Dashboard', url: 'home', icon: 'fas fa-home'},
+  {name: 'Cartypes & Radius', url: 'cartype', icon: 'fas fa-fw fa-car'},
+  //{name: 'Price Changer', url: 'changeprice', icon: 'fas fa-money-check'},
+  //{name: 'App Name', url: 'appname', icon: 'fas fa-pen'}, 
+  {name: 'Payment & Currency', url: 'appcurrency', icon: 'fas fa-dollar'},
+ // {name: 'Country Code & Language', url: 'countrycode', icon: 'fas fa-flag'},
+  //{name: 'Phone Number', url: 'phonenumber', icon: 'fas fa-tablet'},
+  //{name: 'Push Notification & URL', url: 'website', icon: 'fas fa-fw fa-bullhorn'},
+  {name: 'FAQ & Email Notification', url: 'faq', icon: 'fas fa-fw fa-envelope'}, 
+  {name: 'Careers', url: 'careers', icon: 'fas fa-fw fa-suitcase'},
+  //{name: 'Terms & conditions', url: 'termsandservices', icon: 'fas fa fa-drivers-license '},
+  {name: 'Social', url: 'instagram', icon: 'fas fa-video-camera'}, 
+ // {name: 'Features and Password', url: 'change_code', icon: 'fas fa-unlock-alt'},
+  
+ ]
+  }
+
+  ngOnInit() {
+    if (window.screen.width <= 720)
+     this.line = true
+
+     this.ph.getDashboardProfile().on('value', snapshot => {
+      this.username = snapshot.val().username;
+    });
+  }
+
+  check(){
+    this.canHide = this.canHide ? false : true
+  }
+
+  light(){
+    this.isDarkTheme = false;
+  }
+
+  changeColor(){
+    this.isDarkTheme = this.isDarkTheme ? false : true
+  }
+
+
+onResize(event) {
+  this.line = (event.target.innerWidth <= 720) ? true : false;
+  this.canHide = (event.target.innerWidth <= 720) ? true : false;
+}
+
+
+Pop(): void {
+  this.ctrl.name = 'sdgtobjwfbsfiweierihwehhieihuwehiewiuhwethiihweihuewtihwutehiuetiuht'
+    let dialogRef = this.dialog.open(DialogComponent, {
+      width: '700px',
+    });
+ };
+
+
+  hideBar(){
+    console.log("can go home")
+    this.ctrl.sidemenu_toggle = false
+  }
+  
+  sideNav(){
+    this.ctrl.canShowNav = false;
+  }
+  
+  
+  openChange(){
+    this.zone.run(()=>{
+      this.router.navigateByUrl('change_code').then(sucess =>{ 
+    
+      })
+    })
+    
+  }
+
+  goHome(){
+    this.zone.run(()=>{
+    this.router.navigateByUrl('home').then(sucess =>{ 
+  
+    })
+  })
+  
+}
+
+gotoPush(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('website').then(sucess =>{ 
+
+  })
+})
+
+}
+gotoapp(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('appname').then(sucess =>{ 
+
+  })
+})
+
+}
+gotocountry(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('countrycode').then(sucess =>{ 
+
+  })
+})
+
+}
+
+gototerms(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('termsandservices').then(sucess =>{ 
+
+  })
+})
+
+}
+
+gotofeature(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('change_code').then(sucess =>{ 
+
+  })
+})
+
+}
+
+gotoPrice(){
+  this.zone.run(()=>{
+  this.router.navigateByUrl('changeprice').then(sucess =>{ 
+
+  })
+})
+
+}
+
+  
+  gotoPage(page){
+    this.zone.run(()=>{
+    this.router.navigateByUrl(page).then(sucess =>{ 
+      // document.getElementById("container").style.color = "red";
+    })
+  })
+  }
+  
+  logOut(){
+    this.auth.logout()
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,195 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Pipe, Directive, Component } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+import { FormsModule }   from '@angular/forms';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFirestoreDocument } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule} from '@angular/http';
+import { FeedbacksComponent } from './feedbacks/feedbacks.component';
+import { FeedbackDriverComponent } from './feedback-driver/feedback-driver.component';
+import { FeedbackUserComponent } from './feedback-user/feedback-user.component';
+// components
 import { AppComponent } from './app.component';
+import { FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { DriversComponent } from './drivers/drivers.component';
+import { ClientsComponent } from './clients/clients.component';
+import { HomeComponent } from './home/home.component';
+import { DriversWaitingComponent } from './drivers-waiting/drivers-waiting.component';
+import { LoginComponent } from './login/login.component';
+import { BookingComponent } from './booking/booking.component';
+import { ControlService } from './services/control.service';
+import { EmailService } from './services/email.service';
+import { AuthserviceService } from './services/authservice.service';
+import { ProfileService } from './services/profile.service';
+import { UploadServiceService } from './services/upload-service.service';
+import { ChangeCodeComponent } from './change-code/change-code.component';
+import { PayoutHistoryComponent } from './payout-history/payout-history.component';
+import { UserhistoryComponent  } from './userhistory/userhistory.component';
+import { AdditemComponent } from './additem/additem.component';
+import { MoreitemComponent } from './moreitem/moreitem.component';
+import { ManageComponent } from './manage/manage.component';
+import { NgDropFilesDirective } from './directives/ng-drop-files.directive';
+import { ClientDetailsComponent } from './client-details/client-details.component';
+import { DriverDetailsComponent } from './driver-details/driver-details.component';
+import { ChatdriveComponent } from './chatdrive/chatdrive.component';
+import { ChatuserComponent } from './chatuser/chatuser.component';
+import { DriverwaitingDetailsComponent } from './driverwaiting-details/driverwaiting-details.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { DialogUploadComponent } from './upload/upload.component';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CompaniesComponent } from './companies/companies.component';
+import { StartComponent } from './start/start.component';
+import { AddcompanyComponent } from './addcompany/addcompany.component';
+import { AvailableComponent } from './available/available.component';
+import { ProgressComponent } from './progress/progress.component';
+import { MapComponent } from './map/map.component';
+import { CartypeComponent } from './sidemenu/cartype/cartype.component';
+import { AppcurrencyComponent } from './sidemenu/appcurrency/appcurrency.component';
+import { AppnameComponent } from './sidemenu/appname/appname.component';
+import { ChangepriceComponent } from './sidemenu/changeprice/changeprice.component';
+import { CountrycodeComponent } from './sidemenu/countrycode/countrycode.component';
+import { PhonenumberComponent } from './sidemenu/phonenumber/phonenumber.component';
+import { WebsiteComponent } from './sidemenu/website/website.component';
+import { FaqComponent } from './sidemenu/faq/faq.component';
+import { CareersComponent } from './sidemenu/careers/careers.component';
+import { TermsandserviceComponent } from './sidemenu/termsandservice/termsandservice.component';
+import { InstagramComponent } from './sidemenu/instagram/instagram.component';
+// import { PaymentComponent } from './sidemenu/payment/payment.component';
+// import { ReferandearncashComponent } from './sidemenu/referandearncash/referandearncash.component';
+// import { ScheduleridesComponent } from './sidemenu/schedulerides/schedulerides.component';
+
+export const firebaseConfig = {
+ // apiKey: "AIzaSyBODCzP2Hr-ePCkLJNaG2oEJcRTx7U44AU",
+  //authDomain: "https://agcabs-cd9f5.firebaseapp.com",
+ // databaseURL: "https://agcabs-cd9f5.firebaseio.com",
+ // storageBucket: "agcabs-cd9f5.appspot.com",
+ // messagingSenderId: "418606850137",
+ // projectId: 'agcabs-cd9f5'
+
+ apiKey: "AIzaSyAb2-XMBDfc54tR9Ym9RktYIfQTNZCnof4",
+  authDomain: "taxiapp-48ea8.firebaseapp.com",
+  databaseURL: "https://taxiapp-48ea8.firebaseio.com",
+  storageBucket: "taxiapp-48ea8.appspot.com",
+  messagingSenderId: "1003487763108",
+  projectId: 'taxiapp-48ea8'
+};
+
+
+
+
+export const ROUTES: Routes = [
+  // { path: '', component: StartComponent },
+  { path: '', component: LoginComponent },
+  { path: 'driver', component : DriversComponent },
+  { path: 'driver_details', component : DriverDetailsComponent },
+  { path: 'available', component : AvailableComponent },
+  { path: 'progress', component : ProgressComponent },
+  { path: 'client_details', component : ClientDetailsComponent },
+  { path: 'additem', component : AdditemComponent },
+  { path: 'moreitem', component : MoreitemComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'feed_driver', component : FeedbackDriverComponent },
+  { path: 'feed_user', component : FeedbackUserComponent },
+  { path: 'feed', component: FeedbacksComponent },
+  { path: 'history', component: PayoutHistoryComponent },
+  { path: 'driver_pending', component: DriversWaitingComponent },
+  { path: 'chat_driver', component: ChatdriveComponent },
+  { path: 'chat_user', component: ChatuserComponent },
+  { path: 'driver_pending_details', component: DriverwaitingDetailsComponent },
+  { path: 'client', component: ClientsComponent },
+  { path: 'userhistory', component: UserhistoryComponent },
+  { path: 'change_code', component: ChangeCodeComponent },
+  { path: 'manage', component: ManageComponent },
+  { path: 'company', component: CompaniesComponent },
+  { path: 'addcompany', component: AddcompanyComponent },
+  { path: 'phonenumber', component: PhonenumberComponent },
+  { path: 'cartype', component: CartypeComponent },
+  { path: 'appcurrency', component: AppcurrencyComponent },
+  { path: 'appname', component: AppnameComponent},
+  { path: 'careers', component: CareersComponent},
+  { path: 'changeprice', component: ChangepriceComponent },
+  { path: 'countrycode', component: CountrycodeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'instagram', component: InstagramComponent },
+  // { path: 'payment', component: PaymentComponent },
+  // { path: 'referandearncash', component: ReferandearncashComponent},
+  // { path: 'schedulerides', component: ScheduleridesComponent},
+  { path: 'termsandservices', component: TermsandserviceComponent},
+  { path: 'addcompany', component: AddcompanyComponent },
+  { path: 'website', component: WebsiteComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DriversComponent,
+    ClientsComponent,
+    MapComponent,
+    HomeComponent,
+    DriversWaitingComponent,
+    NgDropFilesDirective,
+    AvailableComponent,
+    ProgressComponent,
+    DialogComponent,
+    ManageComponent,
+    UserhistoryComponent,
+    LoginComponent,
+    ChangeCodeComponent,
+    AdditemComponent,
+    DialogUploadComponent,
+    PayoutHistoryComponent,
+    MoreitemComponent,
+    StartComponent,
+    DriverDetailsComponent,
+    DriverwaitingDetailsComponent,
+    ClientDetailsComponent,
+    FeedbackDriverComponent,
+    FeedbackUserComponent,
+    FeedbacksComponent,
+    AddcompanyComponent,
+    BookingComponent,
+    ChatdriveComponent,
+    ChatuserComponent,
+    CompaniesComponent,
+    CartypeComponent,
+    AppcurrencyComponent,
+    AppnameComponent,
+    ChangepriceComponent,
+    CountrycodeComponent,
+    PhonenumberComponent,
+    WebsiteComponent,
+    FaqComponent, 
+    CareersComponent,
+    TermsandserviceComponent,
+    InstagramComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    ChartsModule,
+    HttpModule,
+    FormsModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(ROUTES, {useHash : true}),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ControlService, FormBuilder, AuthserviceService, UploadServiceService, EmailService, ProfileService],
+  bootstrap: [AppComponent],
+  entryComponents: [ DialogComponent, DialogUploadComponent,  ]
 })
 export class AppModule { }
